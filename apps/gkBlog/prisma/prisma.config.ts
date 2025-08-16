cf3dd48ba2919c3714631c0c2e7582955159cc8c
git config --global user.name leafshow“ <381211765@qq.comssh-keygen -t rsa -C 381211765@qq.com>
2025-08-16 12:17:09 +0800

    修正错误

--- apps/gkBlog/prisma/prisma.config.ts
+++ apps/gkBlog/prisma/prisma.config.ts
@@ -1,6 +1,4 @@
 // prisma.config.ts
-import { defineConfig } from "prisma";
-
-export default defineConfig({
-  schema: "./prisma/schema.prisma", // 迁移原有的 schema 配置
-});
+export default {
+  schema: "./prisma/schema.prisma",
+};
